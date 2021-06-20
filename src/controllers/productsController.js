7676dffaf58e1eeb9034bbfee2fb0115b9260f75
Leonardo Galeano <leo22578@gmail.com>
2021-06-20 02:34:58 -0300

    20/06/2021 - rutas productos

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,6 +2,18 @@ const productsController = {
 
     productos: (req, res) => {
         res.render("producto")
+    },
+
+    crearProducto: (req, res) => {
+        res.render("crearProducto")
+    },
+
+    detalleProducto: (req, res) => {
+        res.render("detalleProducto")
+    },
+
+    editarProducto: (req, res) => {
+        res.render("editarProducto")
     }
     
 };
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -5,4 +5,10 @@ const productsController = require ("./../controllers/productsController");
 
 router.get("/producto", productsController.productos);
 
+router.get("/crearProducto", productsController.crearProducto);
+
+router.get("/detalleProducto", productsController.detalleProducto);
+
+router.get("/editarProducto", productsController.editarProducto);
+
 module.exports = router;
